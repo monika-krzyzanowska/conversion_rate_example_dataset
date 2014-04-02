f91b96198611c12450832263f70fa80f71f3de10
monika-krzyzanowska <monika.krzyzanowska89@gmail.com>
2014-04-02 12:54:36 +0200

    Update store_conversion_rate.R

--- store/src/R_scripts/store_conversion_rate.R
+++ store/src/R_scripts/store_conversion_rate.R
@@ -1,3 +1,6 @@
+#This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+#To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+
 #defining campaigns
 campaign1 = "street"  
 campaign2 = "flyer"
